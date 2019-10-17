9d7299f36fd0460e6afc580583219f9ef37e2df6
Mohammed Ali <dody200039@gmail.com>
2019-10-17 16:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ function writeCards(arr,e){
   let arr2=[];
   for(let i=0;i<arr[i].length;i++){
    console.log(`"Thank you,${arr[i]}, for the wonderful ${"e"} gift!",)
+   }
    return arr2;
     
   }
 
-}
+
 
 function countdown( countdown ) {
   while ( countdown > 0 ) {
