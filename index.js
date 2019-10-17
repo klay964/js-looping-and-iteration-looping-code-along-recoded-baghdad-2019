eaa4e96e9a7c2b319d730a0844b60e7705437cbc
Mohammed Ali <dody200039@gmail.com>
2019-10-17 16:19:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let arr=["Ada","Brendan","Ali"];
+function writeCards(arr,"birthday"){
+  for(let i=0;i<arr.length;i++){
+   console.log(`"Thank you,${arr[i]}, for the wonderful ${"birthday"} gift!",)
+    
+  }
+
+}
+writeCards(arr,"birthday");
+
+function countDown(){
+  var n;
+  while (n>0){
+    n--
+    console.log(n-)
+  }
+  countDown()
+  
+}
